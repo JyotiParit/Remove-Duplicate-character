7c9123c1078d0b0529f44ceba930ee6b32595ff3
JyotiParit <jyotiparit88@gmail.com>
2022-01-17 11:51:44 +0530

    Remove Duplicate Char

---
+++ removeDchar.js
@@ -0,0 +1,8 @@
+function removeDChar(string) {
+    return string
+        .split('')
+        .filter(function (item, pos, self) {
+        return self.indexOf(item) == pos;
+    }).join('');
+}
+console.log(removeDChar('tech.at.core'));
